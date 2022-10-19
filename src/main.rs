88f834823cafaee86edd8655d8dfc25c8bd9cfc4
pogyomo <abc_abc_bcd_bcd@yahoo.co.jp>
2022-10-19 15:19:27 +0900

    Clear main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,4 @@
-use lexer::Lexer;
-use parser::Parser;
-
-mod lexer;
-mod parser;
 mod utils;
 
 fn main() {
-    let mut input = String::new();
-    std::io::stdin().read_line(&mut input).unwrap();
-    let lexer = Lexer::new(input.as_str());
-    let mut parse = Parser::new(lexer);
-    println!("{:#?}", parse.parse_stmt());
 }
